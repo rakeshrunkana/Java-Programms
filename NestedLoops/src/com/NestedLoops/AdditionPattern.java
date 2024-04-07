fb19abfbc79ea1c0b055d6c8b5017d85dac8ba6c
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-07 21:56:57 +0530

    Initial Commit.

---
+++ NestedLoops/src/com/NestedLoops/AdditionPattern.java
@@ -0,0 +1,14 @@
+package com.NestedLoops;
+
+public class AdditionPattern {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.print(i + j + " ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
---
+++ NestedLoops/src/com/NestedLoops/SquarePattern.java
@@ -0,0 +1,14 @@
+package com.NestedLoops;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
