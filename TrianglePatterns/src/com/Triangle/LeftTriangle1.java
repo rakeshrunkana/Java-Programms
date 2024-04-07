60f6524198ecb6c53611f35c87cc110704e5fd8c
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-07 21:56:21 +0530

    Initial Commit.

---
+++ TrianglePatterns/src/com/Triangle/LeftTriangle1.java
@@ -0,0 +1,16 @@
+package com.Triangle;
+
+public class LeftTriangle1 {
+
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				count++;
+				System.out.print(count + " ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
