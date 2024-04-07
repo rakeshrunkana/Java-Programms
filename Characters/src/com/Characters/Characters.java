e454f67c575afe1a3fcd14a97ec15d3c33c82bae
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-07 21:48:53 +0530

    Initial Commit.

---
+++ Characters/src/com/Characters/Characters.java
@@ -0,0 +1,13 @@
+package com.Characters;
+
+public class Characters {
+
+	public static void main(String[] args) {
+		System.out.println("Printing the alphabets");
+		for (char c = 'A'; c <= 'Z'; c++) {
+			System.out.print(c + " ");
+
+		}
+	}
+
+}
