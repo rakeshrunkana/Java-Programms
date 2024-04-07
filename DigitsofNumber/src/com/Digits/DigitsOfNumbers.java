c807fc657a5f3fc263517511469b2ac488d15b3c
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-07 21:50:17 +0530

    Initial Commit.

---
+++ DigitsofNumber/src/com/Digits/DigitsOfNumbers.java
@@ -0,0 +1,20 @@
+package com.Digits;
+
+import java.util.Scanner;
+
+public class DigitsOfNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Enter any number ");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int r;
+		while (number > 0) {
+			r = number % 10;
+			number = number / 10;
+			System.out.println(r);
+		}
+		System.out.println(number);
+	}
+
+}
