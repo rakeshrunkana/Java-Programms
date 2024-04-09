038acf5166875ffbc3e9910d6f1d549a524db699
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-09 14:00:01 +0530

    Initial Commit.

---
+++ Methods/src/com/MethodPractise/MethodPractise.java
@@ -0,0 +1,17 @@
+package com.MethodPractise;
+
+public class MethodPractise {
+	static int max(int x, int y) {
+		if (x > y)
+			return x;
+		else
+			return y;
+	}
+
+	public static void main(String[] args) {
+		int a = 10, b = 15, c;
+		c = max(a, b);
+		System.out.println("Max of two numbers is " + c);
+	}
+
+}
