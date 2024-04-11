c05439fb3b2a5951c91f87ab99de636e2f086416
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-11 11:34:17 +0530

    Initial Commit.

---
+++ Inheritance/src/com/Circle1/Circle1.java
@@ -0,0 +1,36 @@
+package com.Circle1;
+
+class Circle {
+	public double radius;
+
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+
+	public double perimter() {
+		return 2 * Math.PI * radius;
+	}
+
+	public double circumference() {
+		return perimter();
+	}
+}
+
+class Cylinder extends Circle {
+	public double height;
+
+	public double volume() {
+		return area() * height;
+	}
+}
+
+public class Circle1 {
+	public static void main(String[] args) {
+		Cylinder c = new Cylinder();
+		c.radius = 7;
+		c.height = 10;
+		System.out.println("Volume " + c.volume());
+		System.out.println("Area " + c.area());
+	}
+
+}
