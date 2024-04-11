047c9a4e3913e222b9339c92e239485926f40352
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-11 12:43:00 +0530

    Initial Commit.

---
+++ GcdofTwoNumbers/src/com/GcdofTwoNumbers/GcdofTwoNumbers.java
@@ -0,0 +1,18 @@
+package com.GcdofTwoNumbers;
+
+public class GcdofTwoNumbers {
+	static int gcd(int m, int n) {
+		while (m != n) {
+			if (m > n)
+				m = m - n;
+			else
+				n = n - m;
+		}
+		return m;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(gcd(25, 15));
+	}
+
+}
