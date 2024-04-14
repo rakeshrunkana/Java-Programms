a4fac52f175f71d49cf37e8a10ab3a2ad5fe2670
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-15 00:32:08 +0530

    Initial Commit

---
+++ AbstractExample/src/com/AbstarctExample/AbstarctExample.java
@@ -0,0 +1,30 @@
+package com.AbstarctExample;
+
+abstract class Super {
+	public Super() {
+		System.out.println("Super constructor");
+	}
+
+	public void meth1() {
+		System.out.println("Meth1 of Super ");
+	}
+
+	abstract public void meth2();
+}
+
+class Sub extends Super {
+	@Override
+	public void meth2() {
+		System.out.println("Sub meth2");
+	}
+}
+
+public class AbstarctExample {
+
+	public static void main(String[] args) {
+		Super s = new Sub();
+		s.meth1();
+		s.meth2();
+	}
+
+}
---
+++ AbstractExample/src/com/AbstarctExample/AbstractExample1.java
@@ -0,0 +1,45 @@
+package com.AbstarctExample;
+
+abstract class Shape {
+	abstract public double perimeter();
+
+	abstract public double area();
+}
+
+class Circle extends Shape {
+	double radius;
+
+	public double perimeter() {
+		return 2 * Math.PI * radius;
+	}
+
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+}
+
+class Rectangle extends Shape {
+	double length;
+	double breadth;
+
+	public double perimeter() {
+		return 2 * (length * breadth);
+	}
+
+	public double area() {
+		return length * breadth;
+	}
+}
+
+public class AbstractExample1 {
+
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle();
+		r.length = 10;
+		r.breadth = 5;
+		Shape s = r;
+		System.out.println(s.area());
+		System.out.println(r.area());
+	}
+
+}
