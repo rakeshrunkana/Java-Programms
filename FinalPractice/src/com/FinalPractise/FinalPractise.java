cc86c980aba7ab7e6bf13c397a1aae07fb36ebaa
rakeshrunkana <rakeshrunkana@gmail.com>
2024-04-15 19:00:21 +0530

    Initial Commit.

---
+++ FinalPractice/src/com/FinalPractise/FinalPractise.java
@@ -0,0 +1,23 @@
+package com.FinalPractise;
+class Test {
+    public final void show()
+    {
+        System.out.println();
+    }
+}
+
+class Test1 extends Test
+{
+    //public void show(){}
+    //gives error as final methods cannot be overloaded.
+}
+public class FinalPractise {
+     
+	public static void main(String[] args) {
+    /*final float PI;
+     PI=3.1425f;
+     System.out.println(PI);
+     */
+ 	}
+
+}
