aa12c623c9ead7db3c092a4c68d6768901968691
rakeshrunkana <rakeshrunkana@gmail.com>
2024-05-15 11:09:53 +0530

    Final commit

---
+++ FinalizeExample/src/com/FinalizeExample/FinalizeExample.java
@@ -0,0 +1,18 @@
+package com.FinalizeExample;
+
+public class FinalizeExample {
+
+	public static void main(String[] args) {
+		FinalizeExample obj = new FinalizeExample();
+		System.out.println("Hashcode is: " + obj.hashCode());
+		obj = null;
+		System.gc();
+		System.out.println("End of the garbage collection");
+	}
+
+	protected void finalize() {
+		System.out.println("Called the finalize() method ");
+
+	}
+
+}
